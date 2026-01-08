70fc3a79ea9d57b05a46c315a4d7c1cf06927e9c
aeweb <xfchd090909@gmail.com>
2026-01-08 17:21:19 +0800

    Update data.js

--- data.js
+++ data.js
@@ -6,8 +6,8 @@ const navData = [
                 title: "资讯",
                 icon: "ri-cup-line",
                 links: [
-                    { name: "爱分享", url: "https://aeafx.netlify.app" },
-                    { name: "爱分享(English)", url: "https://aeafxen.netlify.app" }
+                    { name: "爱分享", url: "https://aeafx.netlify.app", desc: "分享新事件每一刻" },
+                    { name: "爱分享(English)", url: "https://aeafxen.netlify.app", desc: "Share every new event" }
                 ]
             },
             {
@@ -26,16 +26,16 @@ const navData = [
                 title: "内容消费",
                 icon: "ri-netflix-line",
                 links: [
-                    { name: "Bilibili", url: "https://bilibili.com" },
-                    { name: "抖音", url: "https://douyin.com" },
-                    { name: "小红书", url: "https://xiaohongshu.com" }
+                    { name: "Bilibili", url: "https://bilibili.com", desc: "哔哩哔哩 (゜-゜)つロ 干杯~-bilibili" },
+                    { name: "抖音", url: "https://douyin.com", desc: "记录美好生活" },
+                    { name: "小红书", url: "https://xiaohongshu.com", desc: "你的生活兴趣社区" }
                 ]
             },
             {
                 title: "境外专区",
                 icon: "ri-earth-line",
                 links: [
-                    { name: "Tiktok", url: "https://tiktok.com" },
+                    { name: "Tiktok", url: "https://tiktok.com", desc: "Make Your Day" },
                     { name: "YouTube", url: "https://YouTube.com" },
                     { name: "Facebook", url: "https://Facebook.com" },
                     { name: "X", url: "https://X.com" },
